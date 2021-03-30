6cf0162bd13cd01fec87be63580ca821309397eb
harrisafoster <harrisafoster@gmail.com>
2021-03-30 14:22:24 +0200

    Premiers tests pour rendre les données visibles sur les films.

--- static/js/fetch_top20.js
+++ static/js/fetch_top20.js
@@ -1,7 +1,26 @@
 async function fetchTop20() {
-    let reponse = await fetch('http://localhost:5000/top20');
-    let data = await reponse.json();
-    console.log(data);
+    let url = 'http://localhost:5000/top20';
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+/// problem, array type object? forEach isn't working
+async function renderTop20() {
+    let movies = await fetchTop20();
+    let html = '';
+    movies.forEach(movie => {
+        let htmlSegment = `<div class="movie">
+                           <img src="${movie.image_url}" >
+                           <h2>${movie.title} ${movie.imdb_score}</h2>
+                           </div>`;
+        html += htmlSegment;
+    });
+
+let container = document.querySelector('.container');
+container.innerHTML = html;
 }
 
-fetchTop20()
+renderTop20()
