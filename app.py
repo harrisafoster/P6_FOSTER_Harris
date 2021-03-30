0d3edc345688c43e04bd1ad36e536fcbf1c9563b
harrisafoster <harrisafoster@gmail.com>
2021-03-30 12:14:27 +0200

    Création du dossier static pour résoudre des bugs dans l'execution des scripts Améliorations sur les fichiers .py communiquant avec l'API, utilisation des paramètres requests pour nettoyer le code. Changement pour afficher le top 20 au lieu du top 5 avec pagination.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import requests
 import json
-from category import Category
+from oc_movies_api import OcMoviesApi
 
 app = Flask(__name__)
 
@@ -10,19 +10,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/top20/genre/<genre>')
+def query_top20_genre(genre):
+    api = OcMoviesApi()
+    return api.top_20_genre(genre)
 
-@app.route('/<genre>')
-def return_json(genre):
-    api = Category()
-    if genre == 'None':
-        return api.top_16_finder(None)
-    else:
-        return api.top_16_finder(genre)
+@app.route('/top20')
+def query_top20():
+    api = OcMoviesApi()
+    return api.top_20()
 
-## javascripts can access multiple routes to do 4 requests at same time
-## route that filters by category?
-## one route to render page, one route for each category
-## this allows asychronous java requests via flask
-## one route can go to all four categories and return json data
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ oc_movies_api.py
@@ -0,0 +1,28 @@
+import requests
+import json
+from flask import jsonify
+## paramenters to reconsctruct URLS requests documentation
+## get rid of json maker
+class OcMoviesApi:
+    def __init__(self):
+        self.url = 'http://localhost:8000/api/v1/titles/'
+    
+    @staticmethod
+    def json_maker(url, search_parameters):
+        top_20_data = {}
+        for page in ['1', '2', '3', '4']:
+            search_parameters['page'] = page
+            requested_data = requests.get(url, params=search_parameters)
+            top_20_data[page] = json.loads(requested_data.content)
+        return top_20_data
+
+    def top_20_genre(self, genre):
+        search_parameters = {'sort_by': '-imdb_score', 'genre': genre}
+        return self.json_maker(self.url, search_parameters)
+    
+    def top_20(self):
+        search_parameters = {'sort_by': '-imdb_score'}
+        return self.json_maker(self.url, search_parameters)
+## get and return data in these methods, used in flask file
+## flask dans contrôleurs
+## no views, accomplished in javascript
