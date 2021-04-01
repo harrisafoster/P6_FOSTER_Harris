8f9a14f70678c5232af3973cbc3e06903fe3fd0d
harrisafoster <harrisafoster@gmail.com>
2021-04-01 14:34:33 +0200

    Ajout et intégration du package flask_cors pour résoudre des bugs d'accès CORS

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 import requests
 import json
 from oc_movies_api import OcMoviesApi
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/")
 def index():
