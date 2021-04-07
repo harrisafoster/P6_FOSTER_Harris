93e8c0922de78f84f99a808a4ef9b4970b5ee732
harrisafoster <harrisafoster@gmail.com>
2021-04-07 15:48:28 +0200

    Testes variés pour essayer d'insérer les données dans la page HTML.

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,11 @@
 async function fetchTop20Genre(genre) {
     if (genre === 'overall') {
         const response = await fetch(('http://localhost:5000/top20/overall'));
-        const movies = await response.json();
+        var movies = await response.json();
         return movies;
     } else {
         const response = await fetch('http://localhost:5000/top20/genre/' + genre);
-        const movies = await response.json();
+        var movies = await response.json();
         return movies;
     }
 }
@@ -18,9 +18,7 @@ function isolateTop20(genre) {
     }
 }
 
-console.log(isolateTop20('overall'));
-console.log(isolateTop20('action'));
-console.log(isolateTop20('horror'));
-console.log(isolateTop20('animation'));
-
-//['1']['results']['0']
+Promise.all([isolateTop20('overall'), isolateTop20('action'), isolateTop20('horror'), isolateTop20('animation')])
+.then(result => {
+    result.forEach(element => console.log(element));
+})
