f6c691149946cada3df4e0990e23efc2a3d88771
harrisafoster <harrisafoster@gmail.com>
2021-04-20 17:24:25 +0200

    Ajout des docstrings Changement pour rechercher le 'long_description' au lieu du 'description' pour le meilleur film du jour.

--- oc_movies_api.py
+++ oc_movies_api.py
@@ -3,11 +3,25 @@ import json
 
 
 class OcMoviesApi:
+    '''
+    The OcMoviesApi class contains the URL for the OC Movies server
+    as well as all methods (sub-functions) to access the API and 
+    return data.
+
+    Initilaize with OcMoviesApi()
+    '''
     def __init__(self):
         self.url = 'http://localhost:8000/api/v1/titles/'
     
     @staticmethod
     def json_maker(url, search_parameters):
+        '''
+        .json_maker is used to retrieve, parse, and return json data for the 
+        first four pages of a given genre.
+        :type args: str, dict
+        :param '*args': url for accessing API, search parameters for defining
+        search criteria
+        '''
         top_20_data = []
         for page in ['1', '2', '3', '4']:
             search_parameters['page'] = page
@@ -20,19 +34,38 @@ class OcMoviesApi:
         return final_data
 
     def top_20_genre(self, genre):
+        '''
+        .top_20_genre returns json data on the top 20 films of a given genre
+        :param arg: genre of type str used to complete the search parameters
+        when accessing the api
+        :type arg: str
+        '''
         search_parameters = {'sort_by': '-imdb_score', 'genre': genre}
         return self.json_maker(self.url, search_parameters)
     
     def top_20(self):
+        '''
+        .top_20returns json data on the top 20 films overall
+        :param arg: no argument is needed
+        '''
         search_parameters = {'sort_by': '-imdb_score'}
         return self.json_maker(self.url, search_parameters)
     
     def get_film_by_id(self, id):
+        '''
+        .get_film_by_id returns json data on the film matching the given id
+        :param arg: id number of film used to find a specific film in the api
+        :type arg: str
+        '''
         return (requests.get(self.url + str(id))).json()
 
     def top_film_overall(self):
+        '''
+        .top_film_overall returns json data on the top film overall
+        :param arg: no argument is taken
+        '''
         sorted_data = (requests.get(self.url, params={'sort_by': '-imdb_score'})).json()
         topfilm_id = sorted_data['results'][0]['id']
         topfilm = (requests.get(self.url + str(topfilm_id))).json()
-        topfilm_data = {'id': topfilm['id'], 'title': topfilm['title'], 'image_url': topfilm['image_url'], 'description': topfilm['description']}
+        topfilm_data = {'id': topfilm['id'], 'title': topfilm['title'], 'image_url': topfilm['image_url'], 'description': topfilm['long_description']}
         return topfilm_data
